1bfd2aa6d76ad314d778f024f5aab5678ae62d45
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-07-20 19:39:18 +0530

    Learning Data Types in C++

---
+++ data_types.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    // There are Mainly Used 6 Datatypes in C++
+    // Bool for Storing True and False
+    bool a = true;              //Size is 1Byte
+
+    // Char for Storing a Single Character
+    char b = 'a';                //Size is 1Byte
+
+    // int for Storing an Integer between -2147483648 to 2147483647
+    int c = 25;                  //Size is 4Bytes
+
+    // long for storing an Ineger greater then 2147483647
+    long d = 25;                  //Size is 8bytes
+
+    // float for storing float numbers
+    float e = 2.5;              //Size is 4Bytes
+
+    //double for storing float numbers but greater 
+    double f = 452525.23;       //Size is 8Bytes
+}
