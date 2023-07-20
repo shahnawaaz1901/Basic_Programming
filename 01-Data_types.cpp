c7bcb4ee654697a601274b3086073d520f2cca6a
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-07-20 20:08:36 +0530

    Learing Data Types in C++

---
+++ 01-Data_types.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Mainly 6 Data Types Exist in C++
+
+    /*--Storing True of False Value--*/
+    bool a = true;                  //Size is 1Byte
+
+    
+    /*--Storing a Single Character Value--*/
+    char b = 'b';                  //Size is 1Byte
+
+    
+    /*--Storing Integer Value--*/
+    int c = 4;                  //Size is 4Byte
+
+    
+    /*--Storing Large Integer Value--*/
+    long d = 45;                  //Size is 8Byte
+
+    
+    /*--Storing Floating Or Decimal Value--*/
+    float e = 452.2;                  //Size is 4Byte
+
+    
+    /*--Storing Large Floating or Decimal Value--*/
+    double f = 45244.2;                  //Size is 8Byte
+}
