0d0dc1f15421a620c40f4809e1a40d87e9995116
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-07-23 20:50:25 +0530

    More on Conditions

---
+++ 16-What_is_Output.cpp
@@ -0,0 +1,11 @@
+//For what values of the x, the 'if-statement' block will not be executed ?
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // if ((1 + x) != (1 - x))
+    // {
+    //     cout << "In if block";
+    // }
+}
