eb986e35642433b4fbac65e057b01c8e26d650d5
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-07-23 20:32:53 +0530

    Code for Area of Square

--- 06-Area_of_Triangle.cpp
+++ 06-Area_of_Triangle.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main(){
     int base, height;
     cin >> base >> height;
-    float area = 1/2 * base * height
+    float area = 1/2 * base * height;
     cout << area << endl;
 }
