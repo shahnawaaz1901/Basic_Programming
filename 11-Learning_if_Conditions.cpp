25b35987e8dcf4cab3bdb1143789ba8ff1cb9fa2
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-07-23 20:42:26 +0530

    Learn Conditions in C++

---
+++ 11-Learning_if_Conditions.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(a > b){
+        cout << "a Is Greater" << endl;
+    }else{
+        cout << "b Is Greater" << endl;
+    }
+}
