12e780817d2df7ebd84a369aa126b3a4f894a905
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-07-23 20:45:47 +0530

    More On Conditions

---
+++ 13-What_is_Output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 5;
+    if (x < 6)
+        cout <<  "Hello" << endl; 
+    if (x == 5)
+     {
+        cout << "Hi" << endl;
+    }
+    else
+    {
+        cout << "Hey" << endl;
+    }
+}
