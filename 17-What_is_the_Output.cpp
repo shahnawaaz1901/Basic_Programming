34c7664ef6c57b2fcd438e0ad1424b52a471dc90
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-07-24 22:07:54 +0530

    More On Conditions

---
+++ 17-What_is_the_Output.cpp
@@ -0,0 +1,17 @@
+// Predict What is Output
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 10, b = 20, c = 100;
+    if(a <= b || c <= b) {
+        cout << "hello" << endl;
+    }
+    else if(a <= b || a <= c) {
+        cout << "hi" << endl;
+    }
+    else {
+        cout << "hey" << endl;
+    }
+}
+
+// Output is hi because a is less then b and c
