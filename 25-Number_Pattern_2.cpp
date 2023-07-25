7dac19f6446ccbf5c3a32cf69f7328bcf612fe3a
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-07-25 15:06:28 +0530

    Learning Number Pattern 2

---
+++ 25-Number_Pattern_2.cpp
@@ -0,0 +1,38 @@
+/*
+Print Number Pattern in This Fashion
+For n = 4
+   1
+  12
+ 123
+1234
+
+For n = 5
+     1
+    12
+   123
+  1234
+ 12345
+123456
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int i = 1;
+    while(i <= n){
+        int j = 1;
+        int k = 1;
+        while(j <= n - i){
+            cout << " ";
+            j++;
+        }
+        while(k <= i){
+            cout << k;
+            k++;
+        }
+        i++;
+        cout << endl;
+    }
+}
