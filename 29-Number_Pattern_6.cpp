68985927228fc54345f97ece23a298c513488019
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-07-25 15:34:03 +0530

    Learn Number Patterns 6

---
+++ 29-Number_Pattern_6.cpp
@@ -0,0 +1,43 @@
+/*
+Print the following pattern
+
+for N = 4
+
+   1
+  23
+ 345
+4567
+
+for N = 3
+
+  1
+ 23
+345
+
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n){
+        int j = i;
+        int k = 1;
+        int l = 1;
+        while(l <= n - i){
+            cout << " ";
+            l++;
+        }
+        while (k <= i)
+        {
+            cout << j;
+            j++;
+            k++;
+        }
+        i++;
+        cout << endl;
+    }
+}
