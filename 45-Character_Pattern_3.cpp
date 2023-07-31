6451d128f9fb6f8a91fa4f199b2338207371053a
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-07-31 09:37:12 +0530

    Learn Character Patterns

---
+++ 45-Character_Pattern_3.cpp
@@ -0,0 +1,29 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+
+A
+BC
+CDE
+DEFG
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int i = 1;
+    char x = 'A';
+    while(i <= n){
+        int j = 1;
+        while(j <= i){
+            cout << x;
+            j = j + 1;
+            x = x + 1;
+        }
+        i = i + 1;
+        cout << endl;
+    }
+}
