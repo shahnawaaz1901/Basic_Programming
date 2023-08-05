c8fbf8bd8190bb5183a55d5f8d09746eb8434d36
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-05 19:56:37 +0530

    Learn More On Operators

--- 56-What_is_the_Output.cpp
+++ 56-What_is_the_Output.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main()
 {
     int x = 10, y = 1;
-    if (++x > 10 || x / 0 == 0)
+    if (++x > 10 || x / 0 == 0)                 // Here x is Incremented Using PreIncrement, So 11 is Compared
         cout << y;
     else
         cout << x;
 }
 
-// Output is : 
+// Output is : 1
