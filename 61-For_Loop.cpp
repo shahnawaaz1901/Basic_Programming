3eaf62aa287eb469ef71f742b107548232e374b3
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-06 20:54:06 +0530

    Learn For Loop

---
+++ 61-For_Loop.cpp
@@ -0,0 +1,12 @@
+// Print Number From 1 to N using For Loop
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    //In for Loop You Need to add Starting Value after that add Condition after semi color After that Increment or Decrement Value
+    // For Loop Prevent Us fromt Loop hell (Where Loop is Run Infinity)
+    for(int i = 1; i <= n;i++){
+        cout << i << " ";
+    }
+}
