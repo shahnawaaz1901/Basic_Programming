8109d276989ee63254ec2ada98bf5eaf85d0c88d
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-07 21:56:33 +0530

    Learn Nesting In For Loop

---
+++ 66-What_is_the_Output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 2; i = i + 1)
+    {
+        for (int j = 0; j < 2; j = j + 1)
+        {
+            if (j == 1)
+                break;
+            cout << j << " ";
+        }
+    }
+}
+
+
+// Output is : 0 0 (Two Times)
