f9601f1f281aa35d9d8196763420764c63f670a5
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-10 16:26:11 +0530

    Learn Continue & Break

---
+++ 71-What_is_the_Output.cpp
@@ -0,0 +1,16 @@
+/*
+Skip iteration
+
+Which of these jump statements can skip processing remainder of code in its body 
+for a particular iteration ?
+1. break
+2. return
+3. continue             // Correct
+
+Solution Description
+####"break" is used to exit from the current loop.
+####"return" statement is used to exit from the current function.
+####"continue" is used to skip the current iteration of a loop and continue with the next iteration.
+
+*/
+
