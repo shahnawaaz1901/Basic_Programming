42e2730789ae39d7f517a1f23290823e723038aa
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-12 19:25:58 +0530

    Learn More on Scope of Variables

---
+++ 85-What_is_the_Output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        cout << i << " ";
+    }
+    // cout << i << " ";
+}
+
+// At Line Number 9 print Statement Gives Error Because Scope of i is Only Available in for Loop
