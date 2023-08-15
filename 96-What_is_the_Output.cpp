057afc5ef1d0c4d7d6d922819a8e54cc59a13347
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-15 21:33:36 +0530

    Some Questions Around If Else and Logical Operators

---
+++ 96-What_is_the_Output.cpp
@@ -0,0 +1,77 @@
+/*
+If & Else
+
+Find the output.
+int p = 100;
+if(p > 20) {
+    if(p < 20) {
+        cout<<"coding";
+    }
+} else {
+    cout<<"ninjas";
+}
+
+1. coding
+2. ninjas
+3. codingninjas
+4. No Output                                // Correct
+
+*/
+
+/*
+Order the code
+
+Write the correct order(s) of statements.(Multiple)
+
+We want to print "Coding Ninjas" 10 times.
+1.  while(x <= 10) {
+2.  int x = 1;
+3.  x += 1;
+4.  cout<<"Coding Ninjas"<<endl;
+5.  }
+
+1. 2 3 1 4 5
+2. 2 1 3 4 5                                // Correct
+3. 2 1 4 3 5                                // Correct
+4. 1 2 4 3 5
+
+*/
+
+/*
+Predict the output
+
+Figure out the output
+int x = 10;
+while(x >= 0) {
+    x = x - 1;
+    cout<<x;
+    x--;
+}
+1. 97531
+2. 97531-1                          // Correct
+3. 10864
+4. 10987654321
+*/
+
+/*
+And/Or Operator
+
+Please select the correct statement(s) about && and || operators.(Multiple)
+
+1. a && b is true if either a or b is true
+2. (a || b) && c is true if c is true and either a or b is true             // Correct
+3. a && b is false if both a and b are true
+4. a || b is true if either a or b is true                                  // Correct
+
+Solution Description
+The && operator gives the true result if
+both the operands are true.
+
+
+The || operator gives the true result if 
+atleast one of the operand is true.
+
+
+Based upon these two logics, option 2
+and option 4 are correct.
+*/
