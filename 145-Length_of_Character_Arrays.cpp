702d7d46595a58a4e2ce05e4e8746a9e8cf6d9b4
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-27 21:26:26 +0530

    Length of Character Array

--- 145-Length_of_Character_Arrays.cpp
+++ 145-Length_of_Character_Arrays.cpp
@@ -11,6 +11,9 @@ int length(char input[]){
 
 int main(){
     char input[1000];
-    cin >> input;
+    cin >> input;                   // if We Give abcde which length is 5
+    /*
+        input[3] = '\0';            // length Become 3, Because Length is Calculated until NULL, Null Works As Terminator
+    */
     cout << "Length of String Is : " << length(input) << endl;
 }
